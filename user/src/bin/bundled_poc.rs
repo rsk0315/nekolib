bb779f4ea41ee14969a6869b1b77507d714b6070
rsk0315 <rsk0315@icloud.com>
2023-06-25 19:42:10 +0900

    fix

--- user/src/bin/bundled_poc.rs
+++ user/src/bin/bundled_poc.rs
@@ -1,25 +1,32 @@
-use nekolib_bundled::{foo::foo1_fn, macros::qux};
+use nekolib_bundled::{foo::foo1_fn, macros::qux1};
 
 fn main() {
     println!("Hello, world!");
-    qux!();
+    qux1!();
     foo1_fn()
 }
 
 // <bundled>
 mod nekolib_bundled {
     pub mod macros {
-        // #[macro_export]
-        macro_rules! qux {
-            () => {
-                println!("qux!")
-            };
+        pub mod qux {
+            // #[macro_export]
+            macro_rules! qux1 {
+                () => {
+                    println!("qux!")
+                };
+            }
+            // https://stackoverflow.com/questions/26731243
+            pub(crate) use qux1;
         }
-        // https://stackoverflow.com/questions/26731243
-        pub(crate) use qux;
+        #[allow(unused_imports)]
+        pub use qux::*;
     }
     pub mod foo {
-        pub fn foo1_fn() {}
+        pub mod foo1 {
+            pub fn foo1_fn() {}
+        }
+        pub use foo1::*;
     }
 }
 // </bundled>
