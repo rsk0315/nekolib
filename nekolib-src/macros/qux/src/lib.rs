11b1b5cea427667b32e176861768e261385a96ee
rsk0315 <rsk0315@icloud.com>
2023-06-27 22:13:07 +0900

    add macros

--- nekolib-src/macros/qux/src/lib.rs
+++ nekolib-src/macros/qux/src/lib.rs
@@ -4,3 +4,31 @@ macro_rules! qux1 {
         println!("qux!");
     };
 }
+
+#[test]
+macro_rules! qux_test {
+    () => {};
+}
+
+macro_rules! qux_internal {
+    ( $($fn:ident,)* ) => { $(
+        fn $fn() -> i32 {
+            123
+        }
+    )* }
+}
+
+#[macro_export]
+macro_rules! qux_long {
+    ( ($fn:ident, $val:expr) ) => {
+        fn $fn() -> i32 {
+            $val
+        }
+    };
+    ( $( ($fn:ident, $val:expr), )* ) => { $(
+        $crate::qux_long! { ($fn, $val) }
+    )* };
+    ( $( ($fn:ident, $val:expr) ),* ) => { $(
+        $crate::qux_long! { ($fn, $val) }
+    )* };
+}
