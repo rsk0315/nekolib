daf12154b7ec3a90e2b9c6020cee1abf427310a0
rsk0315 <rsk0315@icloud.com>
2023-07-05 00:40:25 +0900

    update

--- nekolib-src/add.zsh
+++ nekolib-src/add.zsh
@@ -14,6 +14,8 @@ fi
 
 if ! [[ -d "$mod" ]]; then
     cargo new --lib "$mod"
+    print 'inner = { path = "../inner" }' >> "$mod/Cargo.toml"
+    print 'use inner::doc_inline_reexport;\n\ndoc_inline_reexport! {\n}' >| "$mod/src/lib.rs"
     pushd ../nekolib-doc
     echo "$mod = { path = \"../nekolib-src/$mod\" }" >> Cargo.toml
     vim -N -i NONE -u NONE -s <(print "jf{a${mod},\x1bZZ") src/lib.rs &>/dev/null
@@ -26,7 +28,7 @@ if [[ -n "$crate" ]] && ! [[ -d "$mod/$crate" ]]; then
     pushd "$mod"
     cargo new --lib "$crate"
     echo "$crate = { path = \"$crate\" }" >> Cargo.toml
-    vim -N -i NONE -u NONE -s <(print "GO    ${crate},\x1bVj%j!sortZZ") src/lib.rs &>/dev/null
+    vim -N -i NONE -u NONE -s <(print "G$%a\n    ${crate},\x1bVGk!sort\nZZ") src/lib.rs &>/dev/null
     cat src/lib.rs
     popd
 fi
