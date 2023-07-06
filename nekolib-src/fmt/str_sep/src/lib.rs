156f0ed8f25d17c108c844c95c1a610d3f71fe4c
rsk0315 <rsk0315@icloud.com>
2023-07-06 13:42:32 +0900

    fmt

--- nekolib-src/fmt/str_sep/src/lib.rs
+++ nekolib-src/fmt/str_sep/src/lib.rs
@@ -1,70 +1,88 @@
-pub struct StrSep<'a, D: ?Sized>(pub &'a D, pub &'a str);
-pub struct SpaceSep<'a, D: ?Sized>(pub &'a D);
-pub struct PerLine<'a, D: ?Sized>(pub &'a D);
-
 use std::fmt;
 
+pub struct SpaceSep<I>(I);
+pub struct PerLine<I>(I);
+pub struct StrSep<'a, I>(I, &'a str);
+
+pub struct SpaceSepUsize1<I>(I);
+pub struct PerLineUsize1<I>(I);
+pub struct StrSepUsize1<'a, I>(I, &'a str);
+
 macro_rules! impl_fmt {
-    ( $( ($fmt:ident, $fn:ident), )* ) => { $(
-        fn $fn<I>(it: I, sep: &str, f: &mut fmt::Formatter<'_>) -> fmt::Result
+    ( $( $fmt:ident )* ) => { $(
+        #[allow(non_snake_case)]
+        fn $fmt<I, T: fmt::$fmt>(iter: I, sep: &str, f: &mut fmt::Formatter<'_>) -> fmt::Result
         where
-            I: IntoIterator,
-            <I as IntoIterator>::Item: fmt::$fmt,
+            I: IntoIterator<Item = T>,
         {
-            let mut it = it.into_iter();
-            if let Some(x) = it.by_ref().next() {
-                fmt::$fmt::fmt(&x, f)?;
-                for x in it {
-                    write!(f, "{}", sep)?;
-                    fmt::$fmt::fmt(&x, f)?;
-                }
+            let mut iter = iter.into_iter();
+            if let Some(first) = iter.by_ref().next() {
+                first.fmt(f)?;
             }
-            Ok(())
+            iter.map(|rest| { write!(f, "{}", sep)?; rest.fmt(f) }).collect()
         }
-        impl<'a, D: 'a> fmt::$fmt for StrSep<'a, D>
+
+        impl<I, T: fmt::$fmt> fmt::$fmt for SpaceSep<I>
         where
-            D: ?Sized,
-            &'a D: IntoIterator,
-            <&'a D as IntoIterator>::Item: fmt::$fmt,
+            I: IntoIterator<Item = T> + Clone,
         {
             fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-                $fn(self.0, self.1, f)
+                $fmt(self.0.clone(), " ", f)
             }
         }
-        impl<'a, D: 'a> fmt::$fmt for SpaceSep<'a, D>
+        impl<I, T: fmt::$fmt> fmt::$fmt for PerLine<I>
         where
-            D: ?Sized,
-            &'a D: IntoIterator,
-            <&'a D as IntoIterator>::Item: fmt::$fmt,
+            I: IntoIterator<Item = T> + Clone,
         {
             fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-                $fn(self.0, " ", f)
+                $fmt(self.0.clone(), "\n", f)
             }
         }
-        impl<'a, D: 'a> fmt::$fmt for PerLine<'a, D>
+        impl<I, T: fmt::$fmt> fmt::$fmt for StrSep<'_, I>
         where
-            D: ?Sized,
-            &'a D: IntoIterator,
-            <&'a D as IntoIterator>::Item: fmt::$fmt,
+            I: IntoIterator<Item = T> + Clone,
         {
             fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-                $fn(self.0, "\n", f)
+                $fmt(self.0.clone(), self.1, f)
             }
         }
     )* }
 }
 
-impl_fmt! {
-    (Display, join_display),
-    (Debug, join_debug),
-    (Octal, join_octal),
-    (LowerHex, join_lower_hex),
-    (UpperHex, join_upper_hex),
-    (Pointer, join_pointer),
-    (Binary, join_binary),
-    (LowerExp, join_lower_exp),
-    (UpperExp, join_upper_exp),
+macro_rules! impl_fmt_usize1 {
+    ( $( $fmt:ident )* ) => { $(
+        impl<I, T> fmt::$fmt for SpaceSepUsize1<I>
+        where
+            I: IntoIterator<Item = T> + Clone,
+            T: std::ops::Add<usize, Output = usize>,
+        {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                $fmt(self.0.clone().into_iter().map(|u| u + 1), " ", f)
+            }
+        }
+        impl<I, T> fmt::$fmt for PerLineUsize1<I>
+        where
+            I: IntoIterator<Item = T> + Clone,
+            T: std::ops::Add<usize, Output = usize>,
+        {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                $fmt(self.0.clone().into_iter().map(|u| u + 1), "\n", f)
+            }
+        }
+        impl<I, T> fmt::$fmt for StrSepUsize1<'_, I>
+        where
+            I: IntoIterator<Item = T> + Clone,
+            T: std::ops::Add<usize, Output = usize>,
+        {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                $fmt(self.0.clone().into_iter().map(|u| u + 1), self.1, f)
+            }
+        }
+)* }
 }
+
+impl_fmt! { Binary Debug Display LowerExp LowerHex Octal Pointer UpperExp UpperHex }
+impl_fmt_usize1! { Debug Display LowerHex Octal UpperHex }
 
 #[test]
 fn sanity_check() {
@@ -83,6 +101,15 @@ fn sanity_check() {
     assert_eq!(format!("{}", PerLine(&a[..1])), "0");
     assert_eq!(format!("{}", PerLine(&a[..2])), "0\n1");
     assert_eq!(format!("{}", PerLine(&a)), "0\n1\n2");
+}
+
+#[test]
+fn iter() {
+    assert_eq!(format!("{}", SpaceSep(0..5)), "0 1 2 3 4");
+    assert_eq!(
+        format!("{}", SpaceSep([0, 1, 2].iter().map(|x| x * 100))),
+        "0 100 200"
+    );
 }
 
 #[test]
@@ -127,4 +154,12 @@ fn formatting() {
     let float = [0.1, 2.34, f64::INFINITY, f64::NAN];
     assert_eq!(format!("{:4}", SpaceSep(&float)), " 0.1 2.34  inf  NaN");
     assert_eq!(format!("{:0.2}", SpaceSep(&float)), "0.10 2.34 inf NaN");
+}
+
+#[test]
+fn usize1() {
+    assert_eq!(format!("{}", SpaceSepUsize1([0, 3, 1, 4, 2])), "1 4 2 5 3");
+    let a = vec![0, 3, 1, 4, 2];
+    assert_eq!(format!("{}", SpaceSepUsize1(&a)), "1 4 2 5 3");
+    assert_eq!(format!("{}", SpaceSepUsize1(a)), "1 4 2 5 3");
 }
