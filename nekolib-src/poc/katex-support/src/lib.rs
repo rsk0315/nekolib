42b5e3b94810f3cdca4fc5639ab169e114de113a
rsk0315 <rsk0315@icloud.com>
2024-01-20 16:00:51 +0900

    fix

--- nekolib-src/poc/katex-support/src/lib.rs
+++ nekolib-src/poc/katex-support/src/lib.rs
@@ -1,11 +1,16 @@
 //! $`\KaTeX`$ support
 //!
-//! $`119\cdot 2^{23}+1`$
-//!
-//! $5\cdot 2^{25}+1$
-//!
-//! ``$`119\cdot 2^{23+1}`$`` → ``$<code>119\cdot 2^{23+1}</code>$`` → $`119\cdot 2^{23+1}`$
-//!
+//! Renders ``$`119\cdot 2^{23}+1`$`` into
+//! $`119\cdot 2^{23}+1`$, while `$5\cdot 2^{25}+1$` into
+//! $5\cdot 2^{25}+1$.
+//! Also
+//! ````text
+//! ```math
+//! f(x) = \int_{-\infty}^\infty
+//! \hat f(\xi)\,e^{2 \pi i \xi x}\, \mathrm{d}\xi
+//! ```
+//! ````
+//! into:
 //! ```math
 //! f(x) = \int_{-\infty}^\infty
 //! \hat f(\xi)\,e^{2 \pi i \xi x}\, \mathrm{d}\xi
