88d5012cba5a03e275a22ff6018710285e31ef1b
rsk0315 <rsk0315@icloud.com>
2024-02-10 17:30:52 +0900

    add macros

--- nekolib-src/ops/monoid/src/lib.rs
+++ nekolib-src/ops/monoid/src/lib.rs
@@ -40,3 +40,228 @@ impl<T: BinaryOp + Associative + Identity + Recip + Commutative>
     CommutativeGroup for T
 {
 }
+
+// macro_rules! def_monoid {
+//     ( $ident:ident = ($ty:ty, $op:expr, $id:expr $(, $marker:ident)* ) ) => {
+//         pub struct $ident;
+//         impl BinaryOp for $ident {
+//             type Set = $ty;
+//             fn op(&self, lhs: &Self::Set, rhs: &Self::Set) -> Self::Set {
+//                 ($op)(lhs, rhs)
+//             }
+//         }
+//         impl Identity for $ident {
+//             fn id(&self) -> Self::Set { ($id)() }
+//         }
+//         $( impl $marker for $ident {} )*
+//     };
+//     ( $ident:ident<$($g:ident : $($b:tt),*),*> = ($ty:ty, $op:expr, $id:expr) ) => {
+//         pub struct $ident<$($g),*>;
+//         impl<$($g:$($b)*),*> BinaryOp for $ident<$($g),*> {
+//             type Set = $ty;
+//             fn op(&self, lhs: &Self::Set, rhs: &Self::Set) -> Self::Set {
+//                 ($op)(lhs, rhs)
+//             }
+//         }
+//         impl<$($g:$($b)*),*> Identity for $ident<$($g),*> {
+//             fn id(&self) -> Self::Set { ($id)() }
+//         }
+//         // $( impl<$($g:$($b),*),*> $marker for $ident<$($g),*> {} )*
+//     };
+// }
+
+#[macro_export]
+macro_rules! def_monoid_generics {
+    (
+        $name:ident[$($gen:tt)*] where [$($where:tt)*] =
+            ($ty:ty, $op:expr, $id:expr)
+    ) => {
+        struct $name<$($gen)*>(std::marker::PhantomData<fn() -> ($($gen,)*)>)
+        where $($where)*;
+        impl<$($gen)*> $name<$($gen)*>
+        where $($where)*
+        {
+            fn new() -> Self { Self(std::marker::PhantomData) }
+        }
+        impl<$($gen)*> BinaryOp for $name<$($gen)*>
+        where $($where)*
+        {
+            type Set = $ty;
+            fn op(&self, lhs: &Self::Set, rhs: &Self::Set) -> Self::Set {
+                ($op)(lhs, rhs)
+            }
+        }
+        impl<$($gen)*> Identity for $name<$($gen)*>
+        where $($where)*
+        {
+            fn id(&self) -> Self::Set { ($id)() }
+        }
+    };
+    (
+        $name:ident[$($gen:tt)*] where [$($where:tt)*] =
+            ($ty:ty, $op:expr, $id:expr, Commutative) $(,)?
+    ) => {
+        def_monoid_generics! {
+            $name[$($gen)*] where [$($where)*] = ($ty, $op, $id)
+        }
+        impl<$($gen)*> Commutative for $name<$($gen)*> where $($where)* {}
+    };
+    (
+        $($name:ident[$($gen:tt)*] where [$($where:tt)*] = ($($impl:tt)*)),*
+    ) => { $(
+        def_monoid_generics! {
+            $name[$($gen)*] where [$($where)*] = ($($impl)*)
+        }
+    )* };
+    (
+        $($name:ident[$($gen:tt)*] where [$($where:tt)*] = ($($impl:tt)*),)*
+    ) => { $(
+        def_monoid_generics! {
+            $name[$($gen)*] where [$($where)*] = ($($impl)*)
+        }
+    )* };
+}
+
+#[macro_export]
+macro_rules! def_group_generics {
+    (
+        $name:ident[$($gen:tt)*] where [$($where:tt)*] =
+            ($ty:ty, $op:expr, $id:expr, $recip:expr) $(,)?
+    ) => {
+        def_monoid_generics! {
+            $name[$($gen)*] where [$($where)*] = ($ty, $op, $id)
+        }
+        impl<$($gen)*> Recip for $name<$($gen)*>
+        where $($where)*
+        {
+            fn recip(&self, lhs: &Self::Set) -> Self::Set { ($recip)(lhs) }
+        }
+    };
+    (
+        $name:ident[$($gen:tt)*] where [$($where:tt)*] =
+            ($ty:ty, $op:expr, $id:expr, $recip:expr, Commutative)
+    ) => {
+        def_group_generics! {
+            $name[$($gen)*] where [$($where)*] = ($ty, $op, $id, $recip)
+        }
+        impl<$($gen)*> Commutative for $name<$($gen)*> where $($where)* {}
+    };
+    (
+        $($name:ident[$($gen:tt)*] where [$($where:tt)*] = ($($impl:tt)*)),*
+    ) => { $(
+        def_group_generics! {
+            $name[$($gen)*] where [$($where)*] = ($($impl)*)
+        }
+    )* };
+    (
+        $($name:ident[$($gen:tt)*] where [$($where:tt)*] = ($($impl:tt)*),)*
+    ) => { $(
+        def_group_generics! {
+            $name[$($gen)*] where [$($where)*] = ($($impl)*)
+        }
+    )* };
+}
+
+#[macro_export]
+macro_rules! def_monoid {
+    ( $name:ident = ($ty:ty, $op:expr, $id:expr) ) => {
+        def_monoid_generics! { $name[] where [] = ($ty, $op, $id) }
+    };
+    ( $($name:ident = ($($impl:tt)*)),* ) => { $(
+        def_monoid! { $name = ($($impl)*) }
+    )* };
+    ( $($name:ident = ($($impl:tt)*),)* ) => { $(
+        def_monoid! { $name = ($($impl)*) }
+    )* };
+}
+
+#[macro_export]
+macro_rules! def_group {
+    ( $name:ident = ($ty:ty, $op:expr, $id:expr, $recip:expr) ) => {
+        def_group_generics! { $name[] where [] = ($ty, $op, $id, $recip) }
+    };
+    ( $($name:ident = ($($impl:tt)*)),* ) => { $(
+        def_group! { $name = ($($impl)*) }
+    )* };
+    ( $($name:ident = ($($impl:tt)*),)* ) => { $(
+        def_group! { $name = ($($impl)*) }
+    )* };
+}
+
+#[cfg(test)]
+mod tests {
+    use std::iter::Sum;
+    use std::ops::{Add, BitXor, Neg};
+
+    use super::*;
+
+    #[test]
+    fn simple_monoid() {
+        def_monoid! {
+            OpXor = (u32, |x, y| x ^ y, || 0),
+            OpAdd = (i32, |x, y| x + y, || 0),
+        }
+
+        let xor = OpXor::new();
+        assert_eq!(xor.id(), 0);
+        assert_eq!(xor.op(&2, &3), 1);
+
+        let add = OpAdd::new();
+        assert_eq!(add.id(), 0);
+        assert_eq!(add.op(&2, &3), 5);
+    }
+
+    #[test]
+    fn simple_group() {
+        def_group! {
+            OpXor = (u32, |x, y| x ^ y, || 0, |&x: &u32| x),
+            OpAdd = (i32, |x, y| x + y, || 0, |&x: &i32| -x),
+        }
+
+        let xor = OpXor::new();
+        assert_eq!(xor.id(), 0);
+        assert_eq!(xor.op(&2, &3), 1);
+
+        let add = OpAdd::new();
+        assert_eq!(add.id(), 0);
+        assert_eq!(add.op(&2, &3), 5);
+    }
+
+    #[test]
+    fn generics_monoid() {
+        def_monoid_generics! {
+            OpXor[T] where [
+                for<'a> &'a T: BitXor<Output = T>,
+                T: for<'a> Sum<&'a T>,
+            ] = (T, |x, y| x ^ y, || None.into_iter().sum(), Commutative),
+        }
+
+        let xor = OpXor::<u64>::new();
+        assert_eq!(xor.id(), 0);
+        assert_eq!(xor.op(&2, &3), 1);
+    }
+
+    #[test]
+    fn generics_group() {
+        def_group_generics! {
+            OpXor[T] where [
+                for<'a> &'a T: BitXor<Output = T>,
+                T: for<'a> Sum<&'a T>,
+            ] = (T, |x, y| x ^ y, || None.into_iter().sum(), |x| &(x ^ x) ^ x, Commutative),
+            OpAdd[T] where [
+                for<'a> &'a T: Add<Output = T> + Neg<Output = T>,
+                T: for<'a> Sum<&'a T>,
+            ] = (T, |x, y| x + y, || None.into_iter().sum(), |x: &T| -x, Commutative),
+        }
+
+        let xor = OpXor::<u64>::new();
+        assert_eq!(xor.id(), 0);
+        assert_eq!(xor.op(&2, &3), 1);
+        assert_eq!(xor.recip(&1), 1);
+
+        let add = OpAdd::<i32>::new();
+        assert_eq!(add.id(), 0);
+        assert_eq!(add.op(&-1, &2), 1);
+        assert_eq!(add.recip(&2), -2);
+    }
+}
