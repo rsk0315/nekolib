90c858d91f7dcd3b50c7027d1ae8a456562276d8
rsk0315 <rsk0315@icloud.com>
2026-08-27 02:39:12 +0900

    u

--- nekolib-src/algo/incremental_fn_table/src/lib.rs
+++ nekolib-src/algo/incremental_fn_table/src/lib.rs
@@ -85,11 +85,65 @@ fn test_binom_acc(n: usize, step: usize) {
     }
 }
 
+#[cfg(test)]
+fn test_binom_x_acc(n: usize, step: usize) {
+    let binom = {
+        let mut dp = vec![vec![0_u128; n + 1]; n + 1];
+        dp[0][0] = 1;
+        for i in 1..=n {
+            dp[i][0] = 1;
+            for j in 1..=i {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            }
+        }
+        dp
+    };
+
+    let binom_x = {
+        let mut dp = binom.clone();
+        for i in 0..=n {
+            for j in 0..=i {
+                dp[i][j] *= j as u128;
+            }
+        }
+        dp
+    };
+
+    let binom_x_acc = {
+        let mut dp = binom_x.clone();
+        for i in 0..=n {
+            for j in 0..n {
+                dp[i][j + 1] += dp[i][j];
+            }
+        }
+        dp
+    };
+
+    let inc_i = |(i, j): (usize, usize), f_ij: &(u128, u128)| {
+        (
+            2 * f_ij.0 - binom[i][j],
+            2 * f_ij.1 + f_ij.0 - binom[i][j] * (j + 1) as u128,
+        )
+    };
+    let inc_j = |(i, j): (usize, usize), f_ij: &(u128, u128)| {
+        (f_ij.0 + binom[i][j + 1], f_ij.1 + binom[i][j + 1] * (j + 1) as u128)
+    };
+
+    let ift = IncrementalFnTable::new(n, step, (1, 0), inc_i, inc_j);
+
+    for i in 0..=n {
+        for j in 0..=n {
+            assert_eq!(ift.get(i, j).1, binom_x_acc[i][j]);
+        }
+    }
+}
+
 #[test]
 fn sanity_check() {
     for n in 1..=100 {
         for step in 1..=n {
             test_binom_acc(n, step);
+            test_binom_x_acc(n, step);
         }
     }
 }
