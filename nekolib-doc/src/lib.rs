ce0a8ffeeaa16deb9cc1f6f62354ea2f2631bdd9
rsk0315 <rsk0315@icloud.com>
2026-09-02 02:29:30 +0900

    a

--- nekolib-doc/src/lib.rs
+++ nekolib-doc/src/lib.rs
@@ -1,3 +1,7 @@
+//! えびちゃん (<a href="https://atcoder.jp/users/rsk0315"><span style="color: #C0C000; font-family: Lato; font-weight: bold">rsk0315</span></a>) のライブラリ。
+//!
+//! 🔗 [GitHub](https://github.com/rsk0315/nekolib) TODO: commit hash も貼る
+
 #[doc(inline)]
 pub use {
     algo,
